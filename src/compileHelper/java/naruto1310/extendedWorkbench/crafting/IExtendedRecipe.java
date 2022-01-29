7061c01726af8929aed95cb05f15194aea47bcfe
glowredman <35727266+glowredman@users.noreply.github.com>
2022-01-29 08:57:17 +0100

    Update Buildscript (#3)
    * Delete .gitmodules
    
    * Update buildscript
    
    * Finish conversion to unified buildscript
    
    * Update build.gradle

--- src/compileHelper/java/naruto1310/extendedWorkbench/crafting/IExtendedRecipe.java
+++ src/compileHelper/java/naruto1310/extendedWorkbench/crafting/IExtendedRecipe.java
@@ -2,7 +2,6 @@ package naruto1310.extendedWorkbench.crafting;
 
 import net.minecraft.item.crafting.IRecipe;
 
-public interface IExtendedRecipe extends IRecipe
-{
+public interface IExtendedRecipe extends IRecipe {
 
 }
